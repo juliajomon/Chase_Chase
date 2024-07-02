f82ac648cdf69a80d0c1def40c19d04f643d294b
Julia Jomon <juliajomon12@gmail.com>
2024-07-02 15:17:19 +0530

    animate screen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "raymath.h"
+
 int main(){
     int width = 384;
     int height = 384;
@@ -8,11 +10,26 @@ int main(){
     Texture2D map = LoadTexture("WorldMap..png");
     Vector2 map_pos{0.0,0.0};
 
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+
+    float speed = 4.0;
+
     while (!WindowShouldClose())
     {
        BeginDrawing();
        ClearBackground(WHITE);
 
+        Vector2 direction{};
+        
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if(IsKeyDown(KEY_D))  direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S))  direction.y += 1.0; 
+        
+        if(Vector2Length(direction) != 0) {
+            map_pos = Vector2Subtract(map_pos,Vector2Scale(Vector2Normalize(direction),speed)) ;
+        }
+       
        DrawTextureEx(map , map_pos , 0, 4.0 , WHITE);
 
        EndDrawing();
